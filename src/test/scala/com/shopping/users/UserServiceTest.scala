b8472f031e65880001c6cb0214a85817fd919375
Pooja Prajapati <poojaprajapati@in-pune-poojap.local>
2019-04-07 23:30:51 +0530

    Correct formatting of userServiceTest class

--- src/test/scala/com/shopping/users/UserServiceTest.scala
+++ src/test/scala/com/shopping/users/UserServiceTest.scala
@@ -34,7 +34,8 @@ class UserServiceTest extends BaseTest with PlayJsonSupport {
     when(repository.getUsers).thenReturn(successful(Right(Seq(user1, user2))))
 
     val mayBeUsers =
-      Await.result(service.getUsers(UserParams(Some(24), None, None)), Duration(3, TimeUnit.SECONDS))
+      Await.result(service.getUsers(UserParams(Some(24), None, None)),
+                   Duration(3, TimeUnit.SECONDS))
 
     mayBeUsers.right.get should contain(user2)
   }
@@ -55,72 +56,77 @@ class UserServiceTest extends BaseTest with PlayJsonSupport {
     when(repository.getUsers).thenReturn(successful(Right(Seq(user1, user2))))
 
     val mayBeUsers =
-      Await.result(service.getUsers(UserParams(None, None, None)), Duration(3, TimeUnit.SECONDS))
+      Await.result(service.getUsers(UserParams(None, None, None)),
+                   Duration(3, TimeUnit.SECONDS))
 
     mayBeUsers.right.get shouldEqual Seq(user1, user2)
   }
 
   test("should sort users in descending order based on their age") {
-      val user1 = User("pooja",
-                       25,
-                       "jewoi@example.com",
-                       "1278993065",
-                       "some random address",
-                       List("pizza"))
-      val user2 = User("pooja",
-                       24,
-                       "jewoi@example.com",
-                       "1278993065",
-                       "some random address",
-                       List("pizza"))
-      when(repository.getUsers).thenReturn(successful(Right(Seq(user2, user1))))
+    val user1 = User("pooja",
+                     25,
+                     "jewoi@example.com",
+                     "1278993065",
+                     "some random address",
+                     List("pizza"))
+    val user2 = User("pooja",
+                     24,
+                     "jewoi@example.com",
+                     "1278993065",
+                     "some random address",
+                     List("pizza"))
+    when(repository.getUsers).thenReturn(successful(Right(Seq(user2, user1))))
 
-      val mayBeUsers =
-        Await.result(service.getUsers(UserParams(None, Some("AGE"), Some("DESC"))), Duration(3, TimeUnit.SECONDS))
+    val mayBeUsers =
+      Await.result(
+        service.getUsers(UserParams(None, Some("AGE"), Some("DESC"))),
+        Duration(3, TimeUnit.SECONDS))
 
-      mayBeUsers.right.get shouldEqual Seq(user1, user2)
-    }
+    mayBeUsers.right.get shouldEqual Seq(user1, user2)
+  }
 
   test("should sort users in ascending order based on their age") {
-      val user1 = User("pooja",
-                       25,
-                       "jewoi@example.com",
-                       "1278993065",
-                       "some random address",
-                       List("pizza"))
-      val user2 = User("pooja",
-                       24,
-                       "jewoi@example.com",
-                       "1278993065",
-                       "some random address",
-                       List("pizza"))
-      when(repository.getUsers).thenReturn(successful(Right(Seq(user1, user2))))
+    val user1 = User("pooja",
+                     25,
+                     "jewoi@example.com",
+                     "1278993065",
+                     "some random address",
+                     List("pizza"))
+    val user2 = User("pooja",
+                     24,
+                     "jewoi@example.com",
+                     "1278993065",
+                     "some random address",
+                     List("pizza"))
+    when(repository.getUsers).thenReturn(successful(Right(Seq(user1, user2))))
 
-      val mayBeUsers =
-        Await.result(service.getUsers(UserParams(None, Some("AGE"), Some("ASC"))), Duration(3, TimeUnit.SECONDS))
+    val mayBeUsers =
+      Await.result(service.getUsers(UserParams(None, Some("AGE"), Some("ASC"))),
+                   Duration(3, TimeUnit.SECONDS))
 
-      mayBeUsers.right.get shouldEqual Seq(user2, user1)
-    }
+    mayBeUsers.right.get shouldEqual Seq(user2, user1)
+  }
 
   test("should by default sort users in ascending order based on their age") {
-      val user1 = User("pooja",
-                       25,
-                       "jewoi@example.com",
-                       "1278993065",
-                       "some random address",
-                       List("pizza"))
-      val user2 = User("pooja",
-                       24,
-                       "jewoi@example.com",
-                       "1278993065",
-                       "some random address",
-                       List("pizza"))
-      when(repository.getUsers).thenReturn(successful(Right(Seq(user1, user2))))
+    val user1 = User("pooja",
+                     25,
+                     "jewoi@example.com",
+                     "1278993065",
+                     "some random address",
+                     List("pizza"))
+    val user2 = User("pooja",
+                     24,
+                     "jewoi@example.com",
+                     "1278993065",
+                     "some random address",
+                     List("pizza"))
+    when(repository.getUsers).thenReturn(successful(Right(Seq(user1, user2))))
 
-      val mayBeUsers =
-        Await.result(service.getUsers(UserParams(None, Some("AGE"), None)), Duration(3, TimeUnit.SECONDS))
+    val mayBeUsers =
+      Await.result(service.getUsers(UserParams(None, Some("AGE"), None)),
+                   Duration(3, TimeUnit.SECONDS))
 
-      mayBeUsers.right.get shouldEqual Seq(user2, user1)
-    }
+    mayBeUsers.right.get shouldEqual Seq(user2, user1)
+  }
 
 }
